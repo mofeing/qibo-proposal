1cb6e5c3e555f6aaa796544d1deb3b893281ca27
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-05-19 16:10:35 +0200

    Implement `add_gate`

--- qibo/circuit.py
+++ qibo/circuit.py
@@ -12,7 +12,9 @@ class Circuit:
         graph = nx.DiGraph()
         head = dict[int, str]()
 
-        graph.add_nodes_from(((f"l{i}i", {"gate": None, "lane": i}) for i in range(nqubits)))
+        graph.add_nodes_from(((f"l{i}{t}", {"gate": None, "lane": i}) for i in range(nqubits) for t in ["i", "o"]))
+
+        graph.add_edges_from(((f"l{i}i", f"l{i}o", {"lane": i}) for i in range(nqubits)))
 
         self.graph = graph
 
@@ -30,15 +32,28 @@ class Circuit:
 
     @singledispatchmethod
     def __iadd__(self, _):
-        raise TypeError()
+        return NotImplemented
 
     @__iadd__.register
-    def _(self, gate: Gate):
-        raise NotImplementedError()
+    def add_gate(self, gate: Gate):
+        assert all(i in range(self.nqubits) for i in gate.qubits)
 
-    @__iadd__.register
-    def _(self, other: Self):
-        if self.nqubits != other.nqubits:
-            raise ValueError("#qubits must match")
+        # create new node for gate
+        new = str(uuid4())
+        self.graph.add_node(new, gate=gate, lane=gate.qubits)
 
-        raise NotImplementedError()
+        # connect new node in between output node and its predecessor (last gate in lane)
+        for lane in gate.qubits:
+            out = f"l{lane}o"
+            last = next(self.graph.predecessors(out))
+            self.graph.remove_edge(last, out)
+            self.graph.add_edge(last, new, lane=lane)
+            self.graph.add_edge(new, out, lane=lane)
+
+
+@Circuit.__iadd__.register(Circuit)
+def _(self, other: Self):
+    if self.nqubits != other.nqubits:
+        raise ValueError("#qubits must match")
+
+    raise NotImplementedError()
